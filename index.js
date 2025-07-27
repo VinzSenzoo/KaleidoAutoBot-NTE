83e2d91a10f98877e945eed4ac02d3cbaed183d6
Vinz Senzo <rinipitria2@gmail.com>
2025-07-27 21:21:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -931,7 +931,7 @@ const configSubmitButton = blessed.button({
     setTimeout(() => {
       try {
         if (!isHeaderRendered) {
-          figlet.text("KALEIDO BOT", { font: "ANSI Shadow" }, (err, data) => {
+          figlet.text("NT EXHAUST", { font: "ANSI Shadow" }, (err, data) => {
             if (!err) headerBox.setContent(`{center}{bold}{cyan-fg}${data}{/cyan-fg}{/bold}{/center}`);
             isHeaderRendered = true;
           });
